f441090223209db244d458a0728e55f8685babe4
Shantanu Sharma <shantanu34@outlook.com>
2022-02-14 13:42:30 +0530

    info check

--- typescript/start.ts
+++ typescript/start.ts
@@ -2,37 +2,42 @@ import Inquirer from "inquirer";
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 // var jj=inquirer
-console.log("-------Welcome to user management tool-------");
+console.log(("-------Welcome to user management tool-------"));
 
+(async () => {
+    while (true) {
+        const answers = await Inquirer
+            .prompt([
+                {
+                    name: "userChoice",
+                    type: "list",
+                    message: "select your choice ?",
+                    choices: ["New Entry", "Find One"]
+                },
+                {
+                    name: 'name',
+                    type: "input",
+                    message: 'What is user full name : '
+                }, {
+                    name: 'email',
+                    type: "input",
+                    message: 'What is user email : '
+                },
+            ]);
+        if (answers.userChoice === "New Entry") {
+            let response = await prisma.user.create({
+                data: {
+                    email: answers.email,
+                    name: answers.name
+                }
+            });
 
-Inquirer
-    .prompt([
-        {
-            name: "userChoice",
-            type: "list",
-            message: "select your choice ?",
-            choices: ["New Entry", "Find One"]
-        },
-        {
-            name: 'name',
-            type: "input",
-            message: 'What is user full name : '
-        }, {
-            name: 'email',
-            type: "input",
-            message: 'What is user email : '
-        },
-    ])
-    .then(async (answers:any) => {
-        if(answers.userChoice==="New Entry"){
-            await prisma.user.create({data:{
-                email:"shantan34@outlook.com",
-                name:"shantanu sharma"
-            }});
-            
-            console.log("saved..");
-        }else{
-            console.log(await prisma.user.findFirst({where:{email:answers.email}}));
+            console.log(("saved.."), response);
+        } else {
+            const data = await prisma.user.findFirst({ where: { email: answers.email } });
+            (data) ? console.log(("user found : "), data) : console.log(("no user found..."));
+
         }
-    });
+    }
+})();
 
